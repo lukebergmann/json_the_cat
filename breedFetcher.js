9cce8ddd2ffe06d9eb228e6637f413a1348c5cc5
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-12 03:48:20 +0000

    created tests, all works

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,24 @@
 const request = require('request');
 
-const breed = process.argv[2];
-
-
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, function(error, response, body) {
-  if (error) {
-   console.log("Error: Undefined cat breed or URL typo");
+const fetchBreedDescription = function(breedName, callback) {
+  if (breedName) {
+    request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function(error, response, body) {
+      if (error) {
+        console.log("Error: Undefined Cat Breed or URL Typo", error);
+      } else {
+        const data = JSON.parse(body);
+        const breed = data[0];
+        if (breed) {
+          callback(null, breed.description);
+        } else {
+          callback('This breed is not found!', null);
+        }
+      }
+    });
   }
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(data[0]["description"]);
+};
 
-});
+module.exports = { fetchBreedDescription };
 
 
 
