033b38e0ac72e224a6564ea7ac8d4f55e0799d91
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-12 01:39:37 +0000

    created breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,39 @@
-const request = require('request')
+const request = require('request');
+
+const breed = process.argv[2];
+
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, function(error, response, body) {
+  if (error) {
+   console.log("Error: Undefined cat breed or URL typo");
+  }
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(data[0]["description"]);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Using template literal to incorperate breed to URL
+// request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
+//   const data = JSON.parse(body);
+//   console.log(data);
+
+//   console.log(data[0]["description"]);
+
+// });
+
+
+
+
